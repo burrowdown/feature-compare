4fa6b659475def656e7040f9ede0a629f4c11428
Danny Burrow <burrowingdan@gmail.com>
2017-06-10 17:16:01 -0700

    can now save products in new rows

--- src/App.js
+++ src/App.js
@@ -1,32 +1,83 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Jumbotron, Table } from 'react-bootstrap'
+import { Jumbotron, Table, Button } from 'react-bootstrap'
 import FeatureTable from './FeatureTable'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      // products: ['stuff', 'things'],
+      products: [],
+      productInput: false,
+      currentInput: ''
+    }
+    this.handleProductButton = this.handleProductButton.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleInputSave = this.handleInputSave.bind(this)
+  }
+  handleProductButton (e) {
+    this.setState({
+      productInput: true
+    })
+  }
+
+  handleInputChange (e) {
+    this.setState({
+      currentInput: e.target.value
+    })
+  }
+
+  handleInputSave (e) {
+    // when state.products is an array of objects:
+    // let newProducts = this.state.products.map((x) => Object.assign({}, x))
+    let newProducts = this.state.products.map((x) => x)
+    newProducts.push(this.state.currentInput)
+    this.setState({
+      products: newProducts,
+      productInput: false
+    })
+  }
+
   render () {
+    let productField = <td><Button id='product-button' onClick={this.handleProductButton}>Add a Product</Button></td>
+    if (this.state.productInput) {
+      productField = <td><input
+        id='new-product-input'
+        onChange={this.handleInputChange}
+        placeholder='Product name'
+        value={this.state.currentInput}
+        />
+        <Button
+          id='save-button'
+          onClick={this.handleInputSave}
+        >save</Button></td>
+    }
+
     return (
       <div className='App'>
-        <Jumbotron>FeatureCompare.com</Jumbotron>
-
+        <Jumbotron><h2>FeatureCompare.com</h2></Jumbotron>
         <Table bordered condensed hover>
           <thead>
             <tr id='new-feature-row'>
               <td />
+              <td />
               <td>
-                <button>Add a feature</button>
+                <Button>Add a feature</Button>
               </td>
             </tr>
           </thead>
-          <tbody>
-            <FeatureTable />
+          <FeatureTable
+            products={this.state.products}
+          />
+          <tfoot>
             <tr id='new-product-row'>
-              <td>
-                <button>Add a product</button>
-              </td>
+              <td />
+              {productField}
               <td />
             </tr>
-          </tbody>
+          </tfoot>
+
         </Table>
       </div>
     )
---
+++ src/tests/FeatureTable.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+import FeatureTable from '../FeatureTable.js'
+
+/* global it describe expect  */
+
+describe('when loading with a "products" array', () => {
+  let testArray = ['Item1', 'Item2', 'Item3']
+  let wrapper = shallow(
+    <FeatureTable
+      products={testArray}
+    />
+  )
+  it('renders a row for each item in the array', () => {
+    expect(wrapper.find('.product-row').length).toBe(3)
+  })
+})
